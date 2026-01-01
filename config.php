f544dc9650ce3d17ac1d23454fcca06411e7ef4a
wilson20042025 <wilso20042025@gmail.com>
2026-01-02 00:43:26 +0530

    update config file

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'host' => 'sql201.infinityfree.com',
-    'dbname' => 'if0_39057357_twentyfirstvisual',
-    'username' => 'if0_39057357',
-    'password' => 'HZSzN5CNp9dqZ'
+    'host' => 'sql313.infinityfree.com',
+    'dbname' => 'if0_40806379_XXX',
+    'username' => 'if0_40806379',
+    'password' => 'R7yyvVJl0wO3v'
 ];
 ?>
